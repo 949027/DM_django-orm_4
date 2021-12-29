baf2861f14e3f1d667b5e4eb4c606a54a649fef4
949027 <82878668+949027@users.noreply.github.com>
2021-12-29 12:20:42 +0700

    Add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,11 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images', null=True, blank=True)
+    level = models.IntegerField(null=True)
+    health = models.IntegerField(null=True)
+    attack = models.IntegerField(null=True)
+    protection = models.IntegerField(null=True)
+    endurance = models.IntegerField(null=True)
 
     def __str__(self):
         return self.title
