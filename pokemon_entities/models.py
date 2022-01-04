adab1d37b4bc895486fddf3486d197359b846d5f
949027 <82878668+949027@users.noreply.github.com>
2022-01-04 15:08:56 +0700

    Added russian field names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,27 +1,27 @@
-from django.db import models  # noqa F401
+from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, null=True)
-    title_jp = models.CharField(max_length=200, null=True)
-    image = models.ImageField(upload_to='images', null=True, blank=True)
-    level = models.IntegerField(null=True)
-    health = models.IntegerField(null=True)
-    attack = models.IntegerField(null=True)
-    protection = models.IntegerField(null=True)
-    endurance = models.IntegerField(null=True)
-    description = models.TextField(null=True)
-    previous_evolution = models.ForeignKey('self', related_name='previous', on_delete=models.PROTECT, null=True, blank=True)
-    next_evolution = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    title = models.CharField('Название (ru)', max_length=200)
+    title_en = models.CharField('Название (en)', max_length=200, null=True)
+    title_jp = models.CharField('Название (jp)',max_length=200, null=True)
+    image = models.ImageField('Изображение', upload_to='images', null=True, blank=True)
+    level = models.IntegerField('Уровень', null=True)
+    health = models.IntegerField('Здоровье', null=True)
+    attack = models.IntegerField('Атака', null=True)
+    protection = models.IntegerField('Защита', null=True)
+    endurance = models.IntegerField('Выносливость', null=True)
+    description = models.TextField('Описание', null=True)
+    previous_evolution = models.ForeignKey('self', verbose_name='Из кого эволюционировал', related_name='previous', on_delete=models.PROTECT, null=True, blank=True)
+    next_evolution = models.ForeignKey('self', verbose_name='В кого эволюционирует',on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    lat = models.FloatField()
-    lon = models.FloatField()
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.PROTECT, null=True)
+    appeared_at = models.DateTimeField('Появляется')
+    disappeared_at = models.DateTimeField('Исчезает')
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.PROTECT, null=True)
