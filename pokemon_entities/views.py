d808f8b79bfed0a2c51e8e5ff3a29c5747c0f1ec
949027 <82878668+949027@users.noreply.github.com>
2022-01-06 16:06:48 +0700

    Fixed bug when there is no record

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,5 +1,5 @@
 import folium
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Pokemon, PokemonEntity
 
@@ -51,7 +51,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    found_pokemon = Pokemon.objects.get(id=pokemon_id)
+    found_pokemon = get_object_or_404(Pokemon, id=pokemon_id)
     pokemon = {
         'pokemon_id': found_pokemon.id,
         'title_ru': found_pokemon.title,
